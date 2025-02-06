90addbe2cf067224004f67f4eca74a186e96115f
SirHitech <keithcarriere3@gmail.com>
2025-02-05 23:07:05 -0500

    parse xml config

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import os
 import wx
+import xml.etree.ElementTree as ET
 
+
+class MIPITextCtrl(wx.TextCtrl):
+
+    def __init__(self, *args, **kw):
+        self.description = kw.pop("description")
+        super().__init__(*args, **kw)
 
 class MIPIConfigFrame(wx.Frame):
 
-    def __init__(self, *args, **kwargs):
-        super(MIPIConfigFrame, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.xml_tree = None
         self.build()
 
     def build(self):
@@ -13,7 +21,6 @@ class MIPIConfigFrame(wx.Frame):
 
         self.build_menu()
         self.build_status_bar()
-        self.build_grid()
 
     def build_menu(self):
         """
@@ -43,34 +50,49 @@ class MIPIConfigFrame(wx.Frame):
 
     def build_grid(self):
 
-        ROW_HEIGHT = 18
+        # xml tree should have been set by opening a file first
+        if not self.xml_tree:
+            return
+
+        ROW_HEIGHT = 20
 
         main_box_sizer = wx.BoxSizer(wx.VERTICAL)
         config_grid = wx.GridBagSizer(hgap=0, vgap=0)
         horizontal_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         # blank space top left
-        config_grid.Add((10, ROW_HEIGHT), pos=(0, 0))
+        config_grid.Add(wx.TextCtrl(self, size=(20, ROW_HEIGHT), style=wx.TE_READONLY), pos=(0, 0))
 
         # headers
-        self.header1 = wx.StaticText(self, label="Property Name")
+        self.header1 = wx.TextCtrl(self, value="Property Name", size=(240, ROW_HEIGHT), style=wx.TE_READONLY)
         config_grid.Add(self.header1, pos=(0, 1))
-        self.header2 = wx.StaticText(self, label="Data Type")
+        self.header2 = wx.TextCtrl(self, value="Data Type", size=(140, ROW_HEIGHT), style=wx.TE_READONLY)
         config_grid.Add(self.header2, pos=(0, 2))
-        self.header3 = wx.StaticText(self, label="Value")
+        self.header3 = wx.TextCtrl(self, value="Value", size=(140, ROW_HEIGHT), style=wx.TE_READONLY)
         config_grid.Add(self.header3, pos=(0, 3))
 
         # rows TODO make this dynamic based on loaded XML
         # TODO make this look prettier (centered text, more grid-like)
-        for row_index in range(1, 4):
-            row_label = wx.StaticText(self, label=str(row_index), size=(20, ROW_HEIGHT))
+        root = self.xml_tree.getroot()
+        row_index = 1
+        for property in root.iter("Property"):
+            row_label = wx.TextCtrl(self, value=str(row_index), size=(20, ROW_HEIGHT), style=wx.TE_READONLY)
             config_grid.Add(row_label, pos=(row_index, 0))
-            name_cell = wx.TextCtrl(self, size=(240, ROW_HEIGHT))
-            datatype_cell = wx.TextCtrl(self, size=(140, ROW_HEIGHT))
-            value_cell = wx.TextCtrl(self, size=(140, ROW_HEIGHT))
+            name_cell = MIPITextCtrl(self, value=property.find("Name").text, size=(240, ROW_HEIGHT), style=wx.TE_READONLY, description=property.find("Description").text)
+            datatype_cell =MIPITextCtrl(self, value=property.find("DataType").text, size=(140, ROW_HEIGHT), style=wx.TE_READONLY, description=property.find("Description").text)
+            value_cell = MIPITextCtrl(self, value=property.find("Value").text, size=(140, ROW_HEIGHT), description=property.find("Description").text)
             config_grid.Add(name_cell, pos=(row_index, 1))
             config_grid.Add(datatype_cell, pos=(row_index, 2))
             config_grid.Add(value_cell, pos=(row_index, 3))
+
+            name_cell.Bind(wx.EVT_ENTER_WINDOW, self.on_hover)
+            name_cell.Bind(wx.EVT_LEAVE_WINDOW, self.on_unhover)
+            datatype_cell.Bind(wx.EVT_ENTER_WINDOW, self.on_hover)
+            datatype_cell.Bind(wx.EVT_LEAVE_WINDOW, self.on_unhover)
+            value_cell.Bind(wx.EVT_ENTER_WINDOW, self.on_hover)
+            value_cell.Bind(wx.EVT_LEAVE_WINDOW, self.on_unhover)
+
+            row_index += 1
 
         # description box on the right
         self.description_box = wx.TextCtrl(self, size=(200,300), style=wx.TE_MULTILINE | wx.TE_READONLY)
@@ -85,19 +107,28 @@ class MIPIConfigFrame(wx.Frame):
     def on_open(self, event):
         """Open a file"""
         self.dir_name = ''
-        file_dialogue = wx.FileDialog(self, "Choose a file", self.dir_name, "", "*.*", wx.FD_OPEN)
+        file_dialogue = wx.FileDialog(self, "Choose a file", self.dir_name, "", "*.xml", wx.FD_OPEN)
         if file_dialogue.ShowModal() == wx.ID_OK:
             self.filename = file_dialogue.GetFilename()
             self.dir_name = file_dialogue.GetDirectory()
-            with open(os.path.join(self.dir_name, self.filename), 'r') as file:
-                pass  # TODO do something
+            self.xml_tree = ET.parse(os.path.join(self.dir_name, self.filename))  # TODO does this need to be in a try block?
         file_dialogue.Destroy()
+
+        self.build_grid()
 
     def on_about(self, event):
         """A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets"""
         dlg = wx.MessageDialog(self, f"MIPI coding sample test\nAuthor: Keith Carriere", "About MIPI configuration editor", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
+
+    def on_hover(self, event):
+        self.description_box.SetLabelText(event.EventObject.description)
+        event.Skip()
+
+    def on_unhover(self, event):
+        self.description_box.SetLabelText(wx.EmptyString)
+        event.Skip()
 
     def on_exit(self, event):
         self.Close(True)
